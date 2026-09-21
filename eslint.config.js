f1a44213a178cd3490631ac8aef8dfade0f93220
Samuel Acosta <samuel.acosta28@eia.edu.co>
2026-09-20 22:35:37 -0500

    chore(backend): setup inicial del proyecto

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+﻿import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        process: "readonly",
+        console: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+];
